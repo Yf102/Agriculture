5f129a68244ddde67488ba6add7a336d358e37ca
Filip Hristov <phristov@intracol.com>
2018-06-15 18:04:09 +0300

    Added composer autoloader

--- config/application.config.php
+++ config/application.config.php
@@ -4,8 +4,8 @@ return array(
     'modules' => require __DIR__ . '/modules.config.php',
     'module_listener_options' => array(
         'module_paths' => array(
-            './module',
-            './vendor',
+            ROOT_DIR . '/module',
+            ROOT_DIR . '/vendor',
         ),
         // local/global config location when needed
         //'config_glob_paths' => array(
---
+++ lib/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filiphristov
+ * Date: 15/06/2018
+ * Time: 16:46
+ */
+
+define('ROOT_DIR', dirname(__DIR__));
+define('PUB_DIR', ROOT_DIR.'/public/');
+define('LIB_DIR', ROOT_DIR.'/lib/');
+define('MODULE_DIR', ROOT_DIR . '/module');
