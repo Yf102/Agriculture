6ef79ee06005609ce83cf0f6e140942a5926b44a
Filip Hristov <phristov@intracol.com>
2018-06-15 18:05:31 +0300

    Redisigned index.php to include zend autoloader download check - Included composer autoload

--- public/index.php
+++ public/index.php
@@ -1,10 +1,20 @@
 <?php
 
-chdir(dirname(__DIR__));
+// Prepare config and root dir
+$loader = require_once (__DIR__ . '/../vendor/autoload.php');
 
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
 }
 
-require 'init_autoloader.php';
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+// Init auto loader
+//$zf2Path = ROOT_DIR . '/vendor/zendframework/zendframework/library';
+//$loader->add('Zend', $zf2Path);
+
+// Check if zend autoloader downloaded
+if (!class_exists('Zend\Loader\AutoloaderFactory')) {
+    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+}
+
+$app_config = require_once(ROOT_DIR . '/config/application.config.php');
+Zend\Mvc\Application::init($app_config)->run();
